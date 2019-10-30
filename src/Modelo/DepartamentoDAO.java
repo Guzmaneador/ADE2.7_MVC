03522ec1a5ee3a6a0f1306374898e0cba643442a
munchi <munchi@munchi-PC>
2019-10-30 18:48:04 +0000

    ->Creando clses principales

--- src/Modelo/DepartamentoDAO.java
+++ src/Modelo/DepartamentoDAO.java
@@ -6,4 +6,11 @@ package Modelo;
  */
 public interface DepartamentoDAO {
     
+    public String insertar(DepartamentoVO departamento);
+    public String borrar(int id);
+    public String actualizar (DepartamentoVO departamento);
+    public DepartamentoVO listaDepartamentos();
+    public DepartamentoVO dameDepartamentoId(int id);
+    
+    
 }
