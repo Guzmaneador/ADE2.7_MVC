bc5a73f35b1c365b11ff600ccb7ebe51cbf91df8
munchi <munchi@munchi-PC>
2019-11-03 19:09:13 +0000

    ->Concetando servidor MySQL (+) ->Creado BD y tablas principales (+) ->Añadir datos a las tablas ~

--- src/Modelo/ModeloImpl.java
+++ src/Modelo/ModeloImpl.java
@@ -21,67 +21,12 @@ public class ModeloImpl implements Modelo{
         
         Statement crearDBTablas = conexion.createStatement();
         int resultadoUpdate;
-        for (int i = 0; i < comandosSQL.size(); i++) {
-//            if (i<=2){
-                resultadoUpdate = crearDBTablas.executeUpdate(comandosSQL.get(i));
-                System.out.println("->"+i);
+        for (String comando : comandosSQL) {
+                 resultadoUpdate = crearDBTablas.executeUpdate(comando);
                  System.out.println(resultadoUpdate);
-//            }else {
-//                ResultSet resultadoTablas = crearDBTablas.executeQuery(comandosSQL.get(i));
-//                        System.out.println(resultadoTablas);
-//            }
         }
-        //crearDataBase();
-        
+
         conexion.close();
     }
 
-    
-    
-    
-    
-    public void crearDataBase(){
-        String comandoUpdateSQL=
-            "DROP DATABASE IF EXISTS empresa;"+
-            "CREATE DATABASE IF NOT EXISTS empresa;";
-        String comandoTablasSQL=
-            "USE empresa;"+    
-                
-            "CREATE TABLE `departamentos` (" +
-            "  `id` int(2) NOT NULL," +
-            "  `nombre` varchar(15) DEFAULT NULL," +
-            "  `ubicacion` varchar(15) DEFAULT NULL" +
-            ") ENGINE=InnoDB DEFAULT CHARSET=latin1;" +
-
-            "CREATE TABLE `empleados` (" +
-            "  `id` int(2) NOT NULL," +
-            "  `nombre` varchar(20) DEFAULT NULL," +
-            "  `oficio` varchar(20) DEFAULT NULL," +
-            "  `fecha_alta` date DEFAULT NULL," +
-            "  `salario` double DEFAULT NULL" +
-            ") ENGINE=InnoDB DEFAULT CHARSET=latin1;";
-        String comandoKeySQL=
-            "ALTER TABLE `departamentos`" +
-            "  ADD PRIMARY KEY (`id`);" +
-
-            "ALTER TABLE `empleados`" +
-            "  ADD PRIMARY KEY (`id`);";
-        
-        try {
-            Statement crearDBTablas = conexion.createStatement();
-            int resultadoUpdate = crearDBTablas.executeUpdate(comandoUpdateSQL);
-                        System.out.println(resultadoUpdate);
-
-            ResultSet resultadoTablas = crearDBTablas.executeQuery(comandoTablasSQL);
-                        System.out.println(resultadoTablas);
-                        
-           int resultadoKey = crearDBTablas.executeUpdate(comandoKeySQL);
-                        System.out.println(resultadoKey);
-
-        } catch (SQLException ex) {
-            System.out.println("---Error al mandar sentencia de crecion de bd y tablas----");
-            Logger.getLogger(ModeloImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-    }
 }
--- src/Vista/VistaImpl.java
+++ src/Vista/VistaImpl.java
@@ -19,18 +19,30 @@ public class VistaImpl implements Vista{
         comandosSQLInicio.add("DROP DATABASE IF EXISTS empresa;");
         comandosSQLInicio.add("CREATE DATABASE IF NOT EXISTS empresa;");
         comandosSQLInicio.add("USE empresa;");
+        
         comandosSQLInicio.add("CREATE TABLE `departamentos` (" +
-                "`id` int(2) NOT NULL PRIMARY KEY," +
+                "`id` MEDIUMINT(9) NOT NULL PRIMARY KEY," +
                 "`nombre` varchar(15) DEFAULT NULL," +
                 "`ubicacion` varchar(15) DEFAULT NULL" +
                 ") ENGINE=InnoDB DEFAULT CHARSET=latin1;");
-        comandosSQLInicio.add("            CREATE TABLE `empleados` (" +
-                "`id` int(2) NOT NULL PRIMARY KEY," +
+        comandosSQLInicio.add("CREATE TABLE `empleados` (" +
+                "`nif` varchar(9) NOT NULL PRIMARY KEY," +
                 "`nombre` varchar(20) DEFAULT NULL," +
+                "`tipo` ENUM('Empleado','Administrativo') DEFAULT NULL," +
                 "`oficio` varchar(20) DEFAULT NULL," +
                 "`fecha_alta` date DEFAULT NULL," +
-                "`salario` double DEFAULT NULL" +
+                "`salario` DECIMAL(6,2) DEFAULT NULL," +
+                "`id` MEDIUMINT(9) NOT NULL" +
                 ") ENGINE=InnoDB DEFAULT CHARSET=latin1;");
+        
+        comandosSQLInicio.add("Insert INTO departamentos VALUES ('10','CONTABILIDAD','SEVILLA');");
+        comandosSQLInicio.add("Insert INTO departamentos VALUES ('20','INVESTIGACION','MADRID');");
+        comandosSQLInicio.add("Insert INTO departamentos VALUES ('30','VENTAS','BARCELONA');");
+        comandosSQLInicio.add("Insert INTO departamentos VALUES ('40','PRODUCCION','BILBAO');");
+        
+        comandosSQLInicio.add("Insert INTO empleados VALUES ('17845789K','Antonio','Empleado','CONTABLE','1996-12-08','1222.56','10');");
+
+
     }
 
     public ArrayList<String> getComandosSQLInicio() {
