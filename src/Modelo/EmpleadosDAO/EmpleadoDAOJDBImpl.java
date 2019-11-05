7d458e1e38df8c75e76c70b42bd70bfa235aa37b
munchi <munchi@munchi-PC>
2019-11-05 00:19:57 +0000

    ->Concetando servidor MySQL (+) ->Crear BD y tablas principales (+) ->Añadir datos a las tablas (+) ->Crear tabla contraseña (+) ->Crear Login ~~

--- src/Modelo/EmpleadosDAO/EmpleadoDAOJDBImpl.java
+++ src/Modelo/EmpleadosDAO/EmpleadoDAOJDBImpl.java
@@ -1,14 +1,29 @@
 package Modelo.EmpleadosDAO;
 
+
+
+
 /**
  *
  * @author Guzman
  */
 public class EmpleadoDAOJDBImpl implements EmpleadoDAO{
+//    BasicDataSource poolConexiones;
+    
     String SQL_SELECT_EMPLEADOS= "SELECT nombre FROM empleados";
     String SQL_SELECT_PASSWORD="SELECT nif FROM empleados WHERE id=?";
+    private final String SQL_UPDATE = "UPDATE empleados SET nif=? nombre=? tipo=? oficio=? fecha_alta=? salario=? id=? WHERE nombre=?";
     
     
+
+    
+    public String insertar(EmpleadoVO empleado) {
+        
+        
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+    }
   
 
 }
